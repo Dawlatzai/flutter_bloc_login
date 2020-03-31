19ea73777aa436f8c847312e0a16f8949db5891a
Dawlatzai <dawlatzai.ghousi@gmail.com>
2020-03-31 16:13:04 +0430

    Add email and password fields

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          _emailField(),
+          _passwordField(),
+        ],
+      ),
+    );
+  }
+
+  Widget _emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          hintText: 'you@example.com', labelText: 'Email Address'),
+    );
+  }
+
+  Widget _passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'password',
+        labelText: 'Password'
       ),
     );
   }
